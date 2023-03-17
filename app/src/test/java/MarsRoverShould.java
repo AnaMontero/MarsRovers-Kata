6391282751845dc018ec7db97ce3beda06ebb272
Ana Montero <anamongar@gmail.com>
2023-03-17 11:25:56 +0100

    refactor: unified tests related to Rover turning left

--- app/src/test/java/MarsRoverShould.java
+++ app/src/test/java/MarsRoverShould.java
@@ -1,5 +1,4 @@
 import marsrovers.kata.MarsRover;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,27 +15,13 @@ public class MarsRoverShould {
 
     @ParameterizedTest
     @CsvSource({
-            "S, E"
+            "S, E",
+            "E, N",
+            "N, W"
     })
-    public void lookEastWhenTurningLeftAndOriginalDirectionIsSouth(String currentOrientation, String expectedOrientation){
+    public void turnLeftDependingOnCurrentPosition(String currentOrientation, String expectedOrientation){
         MarsRover rover = new MarsRover(currentOrientation);
         String actual = rover.turnLeft();
         assertEquals(expectedOrientation, actual);
-    }
-
-    @Test
-    public void lookNorthWhenTurningLeftAndOriginalDirectionIsEast(){
-        var expected = "N";
-        MarsRover rover = new MarsRover("E");
-        String actual = rover.turnLeft();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void lookWestWhenTurningLeftAndOriginalDirectionIsNorth(){
-        var expected = "W";
-        MarsRover rover = new MarsRover("N");
-        String actual = rover.turnLeft();
-        assertEquals(expected, actual);
     }
 }
